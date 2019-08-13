32e5d6a69ae79d52245a79aec002739dc762ed42
egemenc17 <egemenc17@gmail.com>
2019-08-14 00:32:48 +0300

    Add project files.

---
+++ ExampleWork/Controllers/StaffController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExampleWork.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExampleWork.Controllers
+{
+    public class StaffController : Controller
+    {
+        public static List<Staff> staffs;
+        public IActionResult Index()
+        {
+            List<Staff> staffModels = Staff.GetStaffData();
+            if (staffs != null && staffs.Any())
+            {
+                staffModels.AddRange(staffs);
+            }
+            return View(staffModels);
+        }
+        public IActionResult Detail(int id)
+        {
+            List<Staff> staffModels = Staff.GetStaffData();
+            if (staffs != null && staffs.Any())
+            {
+                staffModels.AddRange(staffs);
+            }
+            Staff item = staffModels.Where(x => x.Id == id).FirstOrDefault();
+            return View(item);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Staff model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (staffs == null)
+                {
+                    staffs = new List<Staff>();
+                }
+                staffs.Add(model);
+                return RedirectToAction("Detail", new { id = model.Id });
+            }
+            return View(model);
+        }
+    }
+}
---
+++ ExampleWork/Models/Staff.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExampleWork.Models
+{
+    public class Staff
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double IdentificationNumber { get; set; }
+        public string Department { get; set; }
+        public int Salary { get; set; }
+
+        public static List<Staff> GetStaffData()
+        {
+            var staff1 = new Staff
+            {
+                Id = 1,
+                FirstName = "Egemen",
+                LastName = "Capacioglu",
+                IdentificationNumber = 15975346821,
+                Department= "IT",
+                Salary=7500
+            };
+            var staff2 = new Staff
+            {
+                Id = 2,
+                FirstName = "Hakan",
+                LastName = "Oz",
+                IdentificationNumber = 15975346821,
+                Department = "IT",
+                Salary = 7500
+            };
+            var staff3 = new Staff
+            {
+                Id = 2,
+                FirstName = "Kemal",
+                LastName = "Aslan",
+                IdentificationNumber = 15975346821,
+                Department = "IT",
+                Salary = 7500
+            };
+            List<Staff> sampleData = new List<Staff>();
+            sampleData.Add(staff1);
+            sampleData.Add(staff2);
+            sampleData.Add(staff3);
+            return sampleData;
+        }
+    }
+    
+}
